5d35a430a084875e5d59eb9e77769a15d7326226
joedupor <joedupor@gmail.com>
2023-09-18 14:28:40 -0500

    init commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+        public bool IsAPalindrome(string word)
+        {
+
+            
+                string reverseWord = string.Empty;
+
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reverseWord += word[i];
+                }
+
+                if (word == reverseWord)
+                {
+                return true;
+                }
+
+                
+            
+
+            return false;
+        }
+
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,24 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("level", true)]
+        [InlineData("madam", true)]
+        [InlineData("deified", true)]
+        [InlineData("levels", false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
+            //Arrange
+            WordSmith instance = new WordSmith();
 
+            //Act - this is just a method that calls another method
+            bool actual = instance.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
+
+       
     }
 }
